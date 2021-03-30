f7e87fdfa416876af26a6690f117262af142e1b0
Gaurab Shrestha <gaurabian111@gmail.com>
2021-03-30 09:59:06 +0545

    authentication and place order

--- app.js
+++ app.js
@@ -1,36 +1,69 @@
+require('dotenv').config();
 const express = require('express');
-const Handlebars = require('handlebars');
-var exhbs = require('express-handlebars');
-const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const path = require('path');
-const PORT = process.env.port || 3300;
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const PORT = process.env.port || 4000;
+const mongoose = require('mongoose');
 
 const app = express();
 
-// Set for the static file
-app.use(express.static('public'));
+// Database connection
+mongoose.connect('mongodb://localhost/pizza-tracker', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected....');
+}).catch(() => {
+    console.log('Connection failed.')
+});
 
-// view engine setup
-app.set('view engine', 'hbs');
-app.engine('hbs', exhbs({
-    extname: 'hbs',
-    defaultView: 'main',
-    layoutsDir: __dirname + '/views/layouts',
-    handlebars: allowInsecurePrototypeAccess(Handlebars)
+// Session Store
+let mongoStore = new MongoStore({
+    mongoUrl: 'mongodb://localhost/pizza-tracker',
+    mongooseConnection: connection,
+    collection: 'sessions'
+});
+
+// Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }));
 
-app.get('/', (req, res) => {
-    res.render('home');
+// Passport config
+const passportInit = require('./app/config/passport');
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+// Set for the static file or assets
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Global middlewares
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    res.locals.user = req.user;
+    next();
 });
-app.get('/cart', (req, res) => {
-    res.render('customer/cart');
-});
-app.get('/login', (req, res) => {
-    res.render('auth/login');
-});
-app.get('/register', (req, res) => {
-    res.render('auth/register');
-});
+
+// view engine setup
+app.use(expressLayouts);
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+require('./routes/web')(app);
 
 app.listen(PORT, () => {
     console.log(`The server is running at ${PORT}.`);
